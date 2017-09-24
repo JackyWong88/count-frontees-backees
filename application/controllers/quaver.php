6f08636c881e56deb5184078ddab0e640c1e3569
System Administrator <root@Jackys-MacBook-Pro.local>
2017-09-24 16:39:47 -0500

    created form page, made a slight adjustment to regex, and completed primary function

--- application/controllers/quaver.php
+++ application/controllers/quaver.php
@@ -2,41 +2,38 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Quaver extends CI_Controller {
 	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 * When a POST comes, it triggers the primary function.
+	 * Loads default page.
+	 * 
 	 */
 	public function index()
 	{
-    	$string = 'We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America.';
-    	count_frontees_backees($string);
+    	if($_POST) {
+    		$result = $this->count_frontees_backees($_POST['text']);
+    	}
+    	$this->load->view('quaver');
 	}
 
 	/**
+	 * Uses regular expressions to find all matches starting by grouping
+	 * words starting with 't' and ending with 'e', then by grouping words
+	 * that start with 't', and lastly grouping words ending with 'e'.
+	 * Then echos a message using javascript with the counts.
 	 * 
-	 *
-	 *
-	 *
+	 * @param string $string The text to parse
 	 */
 	private function count_frontees_backees($string) {
-		$regex = '/(t\B.e\b)|(\bt)|(e\b)/i';
+		$regex = '/(\bt.?e\b)|(\bt)|(e\b)/i';
 		preg_match_all($regex, $string, $matches);
 		$te = count(array_filter($matches[1]));
 		$t = count(array_filter($matches[2])) + $te;
 		$e = count(array_filter($matches[3])) + $te;
-		echo $t."<br>";
-		echo $e."<br>";
-		echo $te."<br>";
+
+		echo "<script type='text/javascript'>alert('"
+		.$t." words start with the letter \"t\"\\n"
+		.$e." words end with the letter \"e\"\\n"
+		.$te." words start with the letter \"t\" and end with the letter \"e\""
+		."');</script>";
 		return;
 	}
 }
